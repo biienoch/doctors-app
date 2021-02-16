c6fa6a5f267a3baa2150463404e6b8f8284b2f29
enoch bii <enochcheruiyot98@gmail.com>
2021-02-16 22:36:23 +0300

    first commit to the project

---
+++ applicationlayer/aboutus.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aboutus</title>
+</head>
+<body>
+    <header>
+    <button type="button" onclick='doctorpatient.php'>Home </button>
+
+<button type="button" onclick='aboutus.php'>About </button>
+    </header>
+    <pre>
+        cooperative Hospitals
+        
+    </pre>
+</body>
+</html>
---
+++ applicationlayer/doctorpatient.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Doctor Patient</title>
+	<link rel="stylesheet"  href="doctorpatient.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="id=edge">
+</head>
+
+<body>
+<header>
+<header>
+<button type="button" onclick='doctorpatient.php'>Home </button>
+
+<button type="button" onclick='aboutus.php'>About </button>
+</header>
+</header>
+>
+<h1>Welcome to Cooperative Hospitals</h1>
+<br>
+<h1>We offer quality health care</h1>
+<br>
+<h1>To all we server</h1>
+<br>
+
+	<div class="wrapper">
+		<div class="btn" > <a href="login3.php">
+
+<button type="button" class="buttonA" > Admin</button>
+
+</div>
+
+</div>
+
+
+	<div class="wrapper" >
+	<div class="btn" > <a href="login.php"  >
+<button type="button" class="buttonP">Patient</button>
+</div>
+
+</div>
+
+
+	<div class="wrapper" >
+	<div class="btn" > <a href="login2.php"  >
+<button type="button" class="buttonD">Doctor</button>
+        </a>
+</div>
+
+</div>
+
+	
+
+
+ 
+	
+
+
+
+
+</body>
+</html>
